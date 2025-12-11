e0c92ee10d40db107c8ace67f65b00d330d7d33a
HaoNgo232 <ngogiahao2018@gmail.com>
2025-12-11 09:07:40 +0700

    feat: implement user authentication with environment configuration and validation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const ENV = process.env.NODE_ENV;
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: !ENV ? '.env.development' : `.env.${ENV}`,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production', 'test'),
+        PORT: Joi.number(),
+        DATABASE_URL: Joi.string().required(),
+        GITHUB_CLIENT_ID: Joi.string().required(),
+        GITHUB_CLIENT_SECRET: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        KUBECONFIG: Joi.string().required(),
+        JWT_EXPIRES_IN: Joi.string().default('7d'),
+        FRONTEND_URL: Joi.string(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
